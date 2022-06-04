944b8b3a1179a393dd528ed062e671050b3a4b8e
Cankat <jankatorumanp@gmail.com>
2022-06-04 22:17:15 +0300

    WRITE the right logic TEST the code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, nu) {
+  let strY = "";
+  if (nu < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < nu; i++) {
+      strY += str;
+    }
+    return strY;
+  }
 };
 
 // Do not edit below this line
